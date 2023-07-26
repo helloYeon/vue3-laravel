bde6f8f441da725eb9ba44237d7bd5de1a92ad67
YEON.JINUN / ヨン.ジヌン <gnom878@gmail.com>
2023-07-26 18:25:14 +0900

    a

--- src/resources/js/components/pages/Ckeditor.vue
+++ src/resources/js/components/pages/Ckeditor.vue
@@ -1,15 +1,7 @@
 <template>
-    <div class="container">
-        <p></p>
-        <p>welcome to vue-sample!</p>
-        <h1>{{ state.count }}</h1>
-        <button type="button" @click="btnClick" class="btn btn-primary">
-            Bootstrap btn ({{ state.count }})
-        </button>
-    </div>
     <div class="row g-3 align-items-center">
         <div class="col-auto">
-            <label for="content" class="col-form-label">内容</label>
+            <label for="content" class="col-form-label">みしゃる</label>
         </div>
         <div class="col-auto">
             <ckeditor
@@ -24,24 +16,21 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, onMounted, reactive } from "vue";
+import { defineComponent, onMounted, reactive, ref } from "vue";
+import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
 
 export default defineComponent({
     components: {},
     setup() {
-        const state = reactive({
-            count: 0,
-        });
-
-        const btnClick = (): void => {
-            state.count++;
-        };
-
+        const state = reactive({});
+        const editorData = ref("");
         onMounted(async () => {});
 
         return {
             state,
-            btnClick,
+            editor: ClassicEditor,
+            editorData,
+            editorConfig: {},
         };
     },
 });
