dbfa087151fa8683966d0196cd2551f23a017797
YEON.JINUN / ヨン.ジヌン <gnom878@gmail.com>
2023-07-26 18:20:23 +0900

    cod

--- src/resources/js/route.ts
+++ src/resources/js/route.ts
@@ -1,4 +1,5 @@
 import TopPage from "@/components/pages/TopPage.vue";
+import Ckeditor from "@/components/pages/Ckeditor.vue";
 // import NextPage from "@/pages/NextPage.vue";
 
 import { createRouter, createWebHistory } from "vue-router";
@@ -8,6 +9,11 @@ const routes = [
         path: "/",
         name: "TopPage",
         component: TopPage,
+    },
+    {
+        path: "/ckeditor",
+        name: "ckEditor",
+        component: Ckeditor,
     },
 ];
 
